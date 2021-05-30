73a328908ffeb46fe0307047f7e234bb20462109
Ricky <carmichael901230@gmail.com>
2021-05-30 15:38:50 -0400

    refactor #11

--- LeetcodeAlg_easy.py
+++ LeetcodeAlg_easy.py
@@ -252,7 +252,8 @@ def removeElement(nums, val):
     start, end = 0, len(nums) - 1
     while start <= end:
         if nums[start] == val:
-            nums[start], nums[end], end = nums[end], nums[start], end - 1
+            nums[start], nums[end] = nums[end], nums[start]
+            end -= 1
         else:
             start +=1
     return start
